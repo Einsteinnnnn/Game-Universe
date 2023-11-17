fe3eed5f675abccf7ee70d65d10be98b50c1b465
Lu <yanxinl4@illinois.edu>
2023-11-17 17:36:56 -0600

    :)

--- backend/database.py
+++ backend/database.py
@@ -45,3 +45,41 @@ class MyDatabase:
         except Error as e:
             print(f"Database query error: '{e}'")
             return False
+        
+    def user_login(self, un, pw):
+        q = "SELECT * FROM User_info WHERE username = {} AND password = {}".format(un, pw)
+        result = query(q)
+        if result:
+            print("Login success")
+        else:
+            print("Login fail")
+        return True
+    
+    def username_check(self, un):
+        q  = "SELECT * FROM User_info WHERE username = {}".format(un)
+        result = query(q)
+        if result:
+            return False
+        else:
+            return True
+
+    def valid_userid(self):
+        q = "SELECT COUNT(*) FROM User_info"
+        return self.query(q)
+
+    def user_register(self, un, pw, email, phonenum):
+        userid = self.valid_userid()
+        q = "INSERT INTO User_info VALUES({},{},{},{},{})".format(userid, un, pw, email, phonenum)
+        return self.query(q)
+    
+    def search_by_keyword(self, keyword):
+        q  = "SELECT * FROM Game_info WHERE queryname LIKE '%{}%'".format(keyword)
+        return self.query(q)
+
+    def add_review(self, userid, gameid, review):
+        q = "INSERT INTO Gamereview VALUES({},{},{})".format(userid, gameid, review)
+        return self.query(q)
+    
+    def add_favorite(self, userid, gameid):
+        q = "INSERT INTO Userfavorite VALUES({},{})".format(userid, gameid)
+        return self.query(q)
